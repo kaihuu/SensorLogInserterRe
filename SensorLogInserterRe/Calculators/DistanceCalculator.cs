a2c7c5c0a57d53bb76c9af5e331c8a1b4d3376ab
Daiki <daiki.kawanuma@gmail.com>
2016-03-28 18:54:05 +0900

    add some classes

---
+++ SensorLogInserterRe/Calculators/DistanceCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Device.Location;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class DistanceCalculator
+    {
+
+        public static float CalcDistance(GeoCoordinate geoFirst, GeoCoordinate geoSecond)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ SensorLogInserterRe/Calculators/SpeedCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class SpeedCalculator
+    {
+
+        public static Tuple<GeoCoordinate, GeoCoordinate> CalcSpeed(GeoCoordinate geoFirst, GeoCoordinate geoSecond)
+        {
+            return new Tuple<GeoCoordinate, GeoCoordinate>(new GeoCoordinate(), new GeoCoordinate());
+        }
+
+    }
+}
