0bdc1ff5ff7e16a82d2e7c63162572bbf669542c
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-01 17:00:23 +0900

    add ClimbingResistanceCaluculator

---
+++ SensorLogInserterRe/Calculators/ClimbingResistanceCaluculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class ClimbingResistanceCaluculator
+    {
+        public static double CalcClimbingResistance(double vehicleMass,double theta)
+        {
+            return vehicleMass * Math.Sin(theta) * 9.80665;
+        }
+    }
+}
