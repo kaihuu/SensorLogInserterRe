fbc1aaf7faed2a8d961c044d5a4d306963160de4
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-01 16:59:10 +0900

    add AccResistanceCalculator

---
+++ SensorLogInserterRe/Calculators/AccResistanceCaluculator.cs
@@ -0,0 +1,17 @@
+﻿using SensorLogInserterRe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class AccResistanceCaluculator
+    {
+        public static double CalcAccRessistance(double acc,double vehicleMass,double vehicleRollingMass)
+        {
+            return (vehicleMass+vehicleRollingMass)*acc;
+        }
+    }
+}
