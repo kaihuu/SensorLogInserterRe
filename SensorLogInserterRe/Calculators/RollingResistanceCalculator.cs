7f365defbb8d41085befb674da2b134ff6d47510
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-01 17:23:53 +0900

    add RollingResistanceCalculator

---
+++ SensorLogInserterRe/Calculators/RollingResistanceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class RollingResistanceCalculator
+    {
+        public static double CalcRollingResistance(double myu,double vehicleMass,double theta)
+        {
+            return myu * vehicleMass * Math.Cos(theta)* 9.80665;
+        }
+    }
+}
