a8ccc21d0ee5de2cdd490c4759111ab079a752fe
Daiki <daiki.kawanuma@gmail.com>
2016-04-01 19:49:08 +0900

    add HubenyDistanceCalc class

---
+++ SensorLogInserterRe/Calculators/CalculatorComponents/HubenyDistanceCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators.CalculatorComponents
+{
+    class HubenyDistanceCalculator
+    {
+        private static double LongRadius = 6378137; // 長半径
+        private static double ShortRadius = 6356752.314245; // 短半径
+
+        //ヒュベニの公式
+        private static double CalcHubenyFormula(GeoCoordinate geoFirst, GeoCoordinate geoSecond)
+        {
+            double differenceLattitude = conversionDegreeToRadian(geoFirst.Latitude - geoSecond.Latitude);//緯度の差
+            double differenceLongitude = conversionDegreeToRadian(geoFirst.Longitude - geoSecond.Longitude);//経度の差
+            double M = calcMeridianCurvature(geoFirst.Latitude, geoSecond.Latitude);//子午線曲率半径
+            double N = calcPrimeVerticalCircleCurvature(geoFirst.Latitude, geoSecond.Latitude);//卯酉線曲率半径
+            double yFirst = conversionDegreeToRadian(geoFirst.Latitude);//緯度をラジアンに変換
+            double ySecond = conversionDegreeToRadian(geoSecond.Latitude);//緯度をラジアンに変換
+            double cosMyuY = Math.Cos((yFirst + ySecond) / 2);//緯度の平均値のコサイン
+            double distance = Math.Sqrt(Math.Pow(differenceLattitude * M, 2) + Math.Pow(differenceLongitude * N * cosMyuY, 2));//距離の計算
+            return distance;
+        }
+
+        //子午線曲率半径
+        private static double calcMeridianCurvature(double lattitudeFirst, double lattitudeSecond)
+        {
+
+            double W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
+            double e2 = calcE2();//第一離心率の２乗を計算
+            double M = LongRadius * (1 - e2) / Math.Pow(W, 3);//子午線曲率半径を計算
+            return M;
+        }
+
+        //卯酉線曲率半径
+        private static double calcPrimeVerticalCircleCurvature(double lattitudeFirst, double lattitudeSecond)
+        {
+            double W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
+            double N = LongRadius / W;//卯酉線曲率半径を計算
+            return N;
+        }
+
+        //ヒュベニの公式のWを導出
+        private static double calcW(double lattitudeFirst, double lattitudeSecond)
+        {
+
+            double yFirst = conversionDegreeToRadian(lattitudeFirst);//緯度をラジアンに変換
+            double ySecond = conversionDegreeToRadian(lattitudeSecond);//緯度をラジアンに変換
+            double sinMyuY2 = Math.Pow(Math.Sin((yFirst + ySecond) / 2), 2);//緯度の平均値のサインの２乗
+            double e2 = calcE2(); //第一離心率の２乗
+            double W = Math.Sqrt(1 - e2 * sinMyuY2);//Wを計算
+            return W;
+        }
+
+        //第一離心率の２乗
+        private static double calcE2()
+        {
+            return (Math.Pow(LongRadius, 2) - Math.Pow(ShortRadius, 2)) / Math.Pow(LongRadius, 2);
+        }
+
+        //degreeをradianに変換
+        private static double conversionDegreeToRadian(double degree)
+        {
+            return degree * Math.PI / 180;
+        }
+
+    }
+}
