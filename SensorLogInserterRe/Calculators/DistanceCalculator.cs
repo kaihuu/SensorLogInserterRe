414223a8822d20ed8a70f1943f91c85e6250e478
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-01 16:58:31 +0900

    modify DistanceCalculator

--- SensorLogInserterRe/Calculators/DistanceCalculator.cs
+++ SensorLogInserterRe/Calculators/DistanceCalculator.cs
@@ -18,43 +18,41 @@ namespace SensorLogInserterRe.Calculators
         //ヒュベニの公式
         private static double calcHubenyFormula(GeoCoordinate geoFirst,GeoCoordinate geoSecond)
         {
-            double differenceLongitude, differenceLattitude,M,N,cosMyuY,yFirst,ySecond,distance;
-            differenceLattitude = conversionDegreeToRadian(geoFirst.Latitude - geoSecond.Latitude);//緯度の差
-            differenceLongitude = conversionDegreeToRadian(geoFirst.Longitude - geoSecond.Longitude);//経度の差
-            M = calcMeridianCurvature(geoFirst.Latitude, geoSecond.Latitude);//子午線曲率半径
-            N = calcPrimeVerticalCircleCurvature(geoFirst.Latitude, geoSecond.Latitude);//卯酉線曲率半径
-            yFirst = conversionDegreeToRadian(geoFirst.Latitude);//緯度をラジアンに変換
-            ySecond = conversionDegreeToRadian(geoSecond.Latitude);//緯度をラジアンに変換
-            cosMyuY = Math.Cos((yFirst + ySecond) / 2);//緯度の平均値のコサイン
-            distance = Math.Sqrt(Math.Pow(differenceLattitude * M, 2) + Math.Pow(differenceLongitude * N * cosMyuY, 2));//距離の計算
+            double differenceLattitude = conversionDegreeToRadian(geoFirst.Latitude - geoSecond.Latitude);//緯度の差
+            double differenceLongitude = conversionDegreeToRadian(geoFirst.Longitude - geoSecond.Longitude);//経度の差
+            double M = calcMeridianCurvature(geoFirst.Latitude, geoSecond.Latitude);//子午線曲率半径
+            double N = calcPrimeVerticalCircleCurvature(geoFirst.Latitude, geoSecond.Latitude);//卯酉線曲率半径
+            double yFirst = conversionDegreeToRadian(geoFirst.Latitude);//緯度をラジアンに変換
+            double ySecond = conversionDegreeToRadian(geoSecond.Latitude);//緯度をラジアンに変換
+            double cosMyuY = Math.Cos((yFirst + ySecond) / 2);//緯度の平均値のコサイン
+            double distance = Math.Sqrt(Math.Pow(differenceLattitude * M, 2) + Math.Pow(differenceLongitude * N * cosMyuY, 2));//距離の計算
                 return distance;
         }
         //子午線曲率半径
         private static double calcMeridianCurvature(double lattitudeFirst,double lattitudeSecond)
         {
-            double M,W,e2;
-            W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
-            e2 = calcE2();//第一離心率の２乗を計算
-            M = a * (1 - e2) / Math.Pow(W, 3);//子午線曲率半径を計算
+            
+            double W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
+            double e2 = calcE2();//第一離心率の２乗を計算
+            double M = a * (1 - e2) / Math.Pow(W, 3);//子午線曲率半径を計算
             return M;
         }
         //卯酉線曲率半径
         private static double calcPrimeVerticalCircleCurvature(double lattitudeFirst,double lattitudeSecond)
         {
-            double N, W;
-            W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
-            N = a / W;//卯酉線曲率半径を計算
+            double W = calcW(lattitudeFirst, lattitudeSecond);//Wを計算
+            double N = a / W;//卯酉線曲率半径を計算
             return N;
         }
         //ヒュベニの公式のWを導出
         private static double calcW(double lattitudeFirst,double lattitudeSecond)
         {
-            double W,yFirst,ySecond,sinMyuY2,e2;
-            yFirst = conversionDegreeToRadian(lattitudeFirst);//緯度をラジアンに変換
-            ySecond = conversionDegreeToRadian(lattitudeSecond);//緯度をラジアンに変換
-            sinMyuY2 =Math.Pow(Math.Sin( (yFirst + ySecond) / 2),2);//緯度の平均値のサインの２乗
-            e2 = calcE2(); //第一離心率の２乗
-            W = Math.Sqrt(1-e2*sinMyuY2);//Wを計算
+            
+            double yFirst = conversionDegreeToRadian(lattitudeFirst);//緯度をラジアンに変換
+            double ySecond = conversionDegreeToRadian(lattitudeSecond);//緯度をラジアンに変換
+            double sinMyuY2 =Math.Pow(Math.Sin( (yFirst + ySecond) / 2),2);//緯度の平均値のサインの２乗
+            double e2 = calcE2(); //第一離心率の２乗
+            double W = Math.Sqrt(1-e2*sinMyuY2);//Wを計算
             return W;
         }
         //第一離心率の２乗
@@ -65,9 +63,7 @@ namespace SensorLogInserterRe.Calculators
         //degreeをradianに変換
         private static double conversionDegreeToRadian(double degree)
         {
-            double radian;
-            radian = degree * Math.PI / 180;
-            return radian;
+            return degree * Math.PI / 180;
         }
         
 
