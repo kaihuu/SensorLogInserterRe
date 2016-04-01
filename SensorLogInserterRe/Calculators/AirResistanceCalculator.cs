8fd312d85673319265b53b04a28a6386db5be283
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-01 16:59:48 +0900

    add AirResistanceCalculator

---
+++ SensorLogInserterRe/Calculators/AirResistanceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class AirResistanceCalculator
+    {
+        public static double CalcAirResistance(double myu,double vehicleMass,double theta)
+        {
+            return myu * vehicleMass * Math.Cos(theta) * 9.80665;
+        }
+    }
+}
