398b60f94114bf30c623a9048dbb0ada020fac7a
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-04 15:37:11 +0900

    add CalcAirResistancePower

--- SensorLogInserterRe/Calculators/AirResistanceCalculator.cs
+++ SensorLogInserterRe/Calculators/AirResistanceCalculator.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace SensorLogInserterRe.Calculators
 {
-    class AirResistanceCalculator
+    static class AirResistanceCalculator
     {
-        public static double CalcAirResistance(double rho, double Cd, double FrontProjectedArea, double windSpeed)
+        public static double CalcAirResistanceForce(double rho, double Cd, double frontProjectedArea, double windSpeed)//空気抵抗力
         {
-            return rho * Cd* FrontProjectedArea * Math.Pow(windSpeed, 2) / 2;
+            return rho * Cd * frontProjectedArea * Math.Pow(windSpeed, 2) / 2;
+        }
+
+        public static double CalcAirResistancePower(double rho, double Cd, double frontProjectedArea, double windSpeed, double vehicleSpeed)//空気抵抗による損失エネルギー，kWh/s
+        {
+            return CalcAirResistanceForce(rho, Cd, frontProjectedArea, windSpeed) * vehicleSpeed / 3600 / 1000;
         }
     }
 }
