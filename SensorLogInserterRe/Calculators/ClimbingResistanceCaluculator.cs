66caa2686a5b73047a4b3847acfd19072469b8c3
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-04 15:50:34 +0900

    modify CalcClimbingResistancePower

--- SensorLogInserterRe/Calculators/ClimbingResistanceCaluculator.cs
+++ SensorLogInserterRe/Calculators/ClimbingResistanceCaluculator.cs
@@ -12,9 +12,9 @@ namespace SensorLogInserterRe.Calculators
         {
             return vehicleMass * Math.Sin(theta) * 9.80665;
         }
-        public static double CalcClimbingResistancePower(double vehicleMass, double theta)//登坂抵抗による損失エネルギー,kWh/s
+        public static double CalcClimbingResistancePower(double vehicleMass, double theta, double vehicleSpeed)//登坂抵抗による損失エネルギー,kWh/s
         {
-            return CalcClimbingResistanceForce(vehicleMass, theta) * vehicleMass / 3600 / 1000;
+            return CalcClimbingResistanceForce(vehicleMass, theta) * vehicleSpeed / 3600 / 1000;
         }
     }
 }
