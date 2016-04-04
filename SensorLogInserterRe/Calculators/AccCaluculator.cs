39f738743f60c41d1dbcedb486c4187df1c5da17
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-04 14:20:04 +0900

    add CalcRegeneEnergy,Acc Calculator (unfinished)

---
+++ SensorLogInserterRe/Calculators/AccCaluculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class AccCaluculator
+    {
+        public static double CalcAcc(GeoCoordinate geoBefore, GeoCoordinate geoThis, GeoCoordinate geoAfter)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SensorLogInserterRe/Calculators/RegeneEnergyCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class RegeneEnergyCalculator
+    {
+        public static double CalcRegeneEnergy(double drivingPower, double vehicleSpeed, double maxDrivingForce, double maxDrivingPower)
+        {
+            double regeneEnergy = 1000;
+            if (drivingPower >= 0)
+            {
+               regeneEnergy = 0;
+            }
+            else
+            {
+               
+            }
+            return regeneEnergy;
+        }
+    }
+}
