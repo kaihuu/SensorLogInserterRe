0a9f77c613cdb71d32e099f5441b52ad7105d995
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-04-04 15:49:52 +0900

    add CalcRollingResistancePower

--- SensorLogInserterRe/Calculators/RollingResistanceCalculator.cs
+++ SensorLogInserterRe/Calculators/RollingResistanceCalculator.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace SensorLogInserterRe.Calculators
 {
-    class RollingResistanceCalculator
+    static class RollingResistanceCalculator
     {
-        public static double CalcRollingResistance(double myu, double vehicleMass, double theta)
+        public static double CalcRollingResistanceForce(double myu, double vehicleMass, double theta)//転がり抵抗力
         {
             return myu * vehicleMass * Math.Cos(theta)* 9.80665;
+        }
+        public static double CalcRollingResistancePower(double myu, double vehicleMass, double theta, double vehicleSpeed)//転がり抵抗による損失エネルギー,kWh/s
+        {
+            return CalcRollingResistanceForce(myu, vehicleMass, theta) * vehicleSpeed / 1000 / 3600;
         }
     }
 }
