0e44580b387d7785aaa7d27108158a979972819c
Daiki <daiki.kawanuma@gmail.com>
2016-05-23 16:09:06 +0900

    temp implementing ...

--- SensorLogInserterRe/Inserters/GpsInserter.cs
+++ SensorLogInserterRe/Inserters/GpsInserter.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SensorLogInserterRe.Utils;
 
 namespace SensorLogInserterRe.Inserters
 {
     class GpsInserter
     {
+        private static readonly int DriverIndex = 4;
+        private static readonly int CarIndex = 5;
+        private static readonly int SensorIndex = 6;
+
         public static void InsertGps(List<string> insertFileList)
         {
-            
+            foreach (var fileName in insertFileList)
+            {
+                string[] word = fileName.Split('\\');
+
+                var gpsRawTable = DataTableUtil.GetAndroidGpsRawTable();
+
+                string driverName = word[DriverIndex];
+                string carName = word[CarIndex];
+                string sensorName = word[SensorIndex];
+            }
         }
 
         private static void InsertGpsRaw()
