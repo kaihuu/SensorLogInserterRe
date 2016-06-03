32016df5734ad09eeee946f1f7f9efce8d42d8a9
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-06-03 16:14:54 +0900

    modify RegeneLossCalculator

--- SensorLogInserterRe/Calculators/RegeneLossCalculator.cs
+++ SensorLogInserterRe/Calculators/RegeneLossCalculator.cs
@@ -9,10 +9,11 @@ namespace SensorLogInserterRe.Calculators
 {
     static class RegeneLossCalculator
     {
-        public static double CalcEnergy(double drivingPower,double regeneEnergy,Car car, double vehicleSpeed)
+        public static double CalcEnergy(double drivingPower,double regeneEnergy,Car car, double vehicleSpeed, double inverterEfficiency)
         {
             double drivingTorque = drivingPower * 1000 * 3600 / vehicleSpeed / 3.6 * car.TireRadius / car.ReductionRatio;
-            double regeneLoss = Math.Abs(drivingPower - regeneEnergy / EfficiencyCalculator.GetInstance().GetEfficiency(car, vehicleSpeed, drivingTorque));
+
+            double regeneLoss = Math.Abs(drivingPower - regeneEnergy / EfficiencyCalculator.GetInstance().GetEfficiency(car, vehicleSpeed, drivingTorque) *100 /inverterEfficiency);
 
             return regeneLoss;
         }
