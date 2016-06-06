780ef09e63a3c1c510815dd99ce47ff53046ac28
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-06-06 16:15:23 +0900

    add ConsumedEnergyCaluculator

---
+++ SensorLogInserterRe/Calculators/ConsumedEnergyCaluculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorLogInserterRe.Models;
+
+namespace SensorLogInserterRe.Calculators
+{
+    static class ConsumedEnergyCaluculator
+    {
+        static public double CalcEnergy(double drivingPower, Car car, double vehicleSpeed, double inverterEfficiency, double maxDrivingForce, double maxDrivingPower)
+        {
+            double consumedEnergy;
+            double drivingForce = drivingPower * 1000 * 3600 / vehicleSpeed / 3.6;
+            double drivingTorque = drivingForce * car.TireRadius / car.ReductionRatio;
+            if (drivingPower >= 0)//力行時
+            {
+                consumedEnergy = drivingPower / EfficiencyCalculator.GetInstance().GetEfficiency(car, vehicleSpeed, drivingTorque) * 100 / inverterEfficiency;
+            }
+            else//回生時
+            {
+                consumedEnergy = RegeneEnergyCalculator.CalcEnergy(drivingPower, vehicleSpeed, maxDrivingForce, maxDrivingPower, car, inverterEfficiency);
+            }
+            return consumedEnergy;
+        }
+    }
+}
