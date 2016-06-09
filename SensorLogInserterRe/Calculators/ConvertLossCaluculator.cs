f49bf929f3d5244e1edcb0790deaff3fd7b13c14
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-06-09 14:14:53 +0900

    add ConvertLossCaluculator

---
+++ SensorLogInserterRe/Calculators/ConvertLossCaluculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorLogInserterRe.Models;
+
+namespace SensorLogInserterRe.Calculators
+{
+    static class ConvertLossCaluculator
+    {
+        static public double CalcEnergy(double drivingPower, Car car, double vehicleSpeed, double inverterEfficiency, double maxDrivingForce, double maxDrivingPower)
+        {
+            double convertLoss;
+            double drivingForce = drivingPower * 1000 * 3600 / vehicleSpeed / 3.6;
+            double drivingTorque = drivingForce * car.TireRadius / car.ReductionRatio;
+            if(drivingPower >= 0)
+            {
+                convertLoss = ConsumedEnergyCaluculator.CalcEnergy(drivingPower, car, vehicleSpeed, inverterEfficiency, maxDrivingForce, maxDrivingPower)
+                    * (1 - EfficiencyCalculator.GetInstance().GetEfficiency(car, vehicleSpeed, drivingTorque) / 100 * inverterEfficiency);
+            }
+            else
+            {
+                convertLoss = ConsumedEnergyCaluculator.CalcEnergy(drivingPower, car, vehicleSpeed, inverterEfficiency, maxDrivingForce, maxDrivingPower)
+                    * (1 - 1 / EfficiencyCalculator.GetInstance().GetEfficiency(car, vehicleSpeed, drivingTorque) * 100 / inverterEfficiency);
+            }
+            return convertLoss;
+        }
+    }
+}
