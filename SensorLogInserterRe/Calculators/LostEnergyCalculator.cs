adcd87306201f88fa929d66a14adb700281b90e2
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-06-09 14:26:22 +0900

    add LostEnergyCalculator

---
+++ SensorLogInserterRe/Calculators/LostEnergyCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorLogInserterRe.Models;
+
+namespace SensorLogInserterRe.Calculators
+{
+    static class LostEnergyCalculator
+    {
+        static public double CalcEnergy(double drivingPower, Car car, double vehicleSpeed, double inverterEfficiency, double maxDrivingForce, double maxDrivingPower,
+            double rho, double windspeed, double myu, double theta)
+        {
+            double regeneEnergy = RegeneEnergyCalculator.CalcEnergy(drivingPower, vehicleSpeed, maxDrivingForce, maxDrivingPower, car, inverterEfficiency);
+            return Math.Abs(ConvertLossCaluculator.CalcEnergy(drivingPower ,car, vehicleSpeed, inverterEfficiency, maxDrivingForce, maxDrivingPower))
+                + RegeneLossCalculator.CalcEnergy(drivingPower,regeneEnergy,car,vehicleSpeed,inverterEfficiency)
+                + AirResistanceCalculator.CalcPower(rho, car.CdValue, car.FrontalProjectedArea, windspeed, vehicleSpeed)
+                + RollingResistanceCalculator.CalcPower(myu, car.Weight, theta, vehicleSpeed);
+        }
+    }
+}
