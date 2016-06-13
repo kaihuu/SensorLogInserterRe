20573d51c147f2ece5966d3e689d803c78da68db
Daiki <daiki.kawanuma@gmail.com>
2016-06-13 23:20:49 +0900

    implement LogWriter

--- SensorLogInserterRe/Utils/LogWritter.cs
+++ SensorLogInserterRe/Utils/LogWritter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,87 @@ namespace SensorLogInserterRe.Utils
 {
     class LogWritter
     {
+        public enum LogMode
+        {
+            Gps,
+            Acc,
+            Trip,
+            Ecolog,
+            Error
+        }
+
+        private static readonly string DirectoryName = "LogFiles";
+
+        public static void WriteLog(LogMode mode, string text)
+        {
+            LogWritter.CreateDirectory();
+            string fileName;
+
+            switch (mode)
+            {
+                case LogMode.Gps:
+
+                    fileName = $@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}\{DateTime.Now.ToString("HH")}_gps.log";
+
+                    using (StreamWriter writer = new StreamWriter(fileName, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " : " + text);
+                    }
+                    break;
+
+                case LogMode.Acc:
+
+                    fileName = $@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}\{DateTime.Now.ToString("HH")}_acc.log";
+
+                    using (StreamWriter writer = new StreamWriter(fileName, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " : " + text);
+                    }
+                    break;
+
+                case LogMode.Trip:
+
+                    fileName = $@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}\{DateTime.Now.ToString("HH")}_trip.log";
+
+                    using (StreamWriter writer = new StreamWriter(fileName, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " : " + text);
+                    }
+                    break;
+
+                case LogMode.Ecolog:
+
+                    fileName = $@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}\{DateTime.Now.ToString("HH")}_ecolog.log";
+
+                    using (StreamWriter writer = new StreamWriter(fileName, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " : " + text);
+                    }
+                    break;
+
+                case LogMode.Error:
+
+                    fileName = $@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}\{DateTime.Now.ToString("HH")}_error.log";
+
+                    using (StreamWriter writer = new StreamWriter(fileName, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " : " + text);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static void CreateDirectory()
+        {
+            if (!Directory.Exists(DirectoryName))
+                Directory.CreateDirectory(DirectoryName);
+
+            if (!Directory.Exists($@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}"))
+                Directory.CreateDirectory($@"{DirectoryName}\{DateTime.Now.ToString("yyyy-MM-dd")}");
+        }
+
     }
 }
