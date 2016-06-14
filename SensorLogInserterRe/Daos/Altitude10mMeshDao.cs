ec1b4c989bdc0438f8e40ee0e1d7781c4419c1df
Daiki-Kawanuma <daiki.kawanuma@gmail.com>
2016-06-14 13:40:17 +0900

    implement daos

---
+++ SensorLogInserterRe/Daos/Altitude10mMeshDao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Effects;
+
+namespace SensorLogInserterRe.Daos
+{
+    class Altitude10MMeshDao
+    {
+        private static readonly string TableName = "altitude_10m_mesh";
+        public static readonly string ColumnLowerLatitude = "lower_latitude";
+        public static readonly string ColumnLowerLongitude = "lower_longitude";
+        public static readonly string ColumnUpperLatitude = "upper_latitude";
+        public static readonly string ColumnUpperLongitude = "upper_longitude";
+        public static readonly string ColumnAltitude = "altitude";
+
+    }
+}
---
+++ SensorLogInserterRe/Daos/Altitude10mMeshRegisteredDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Daos
+{
+    class Altitude10mMeshRegisteredDao
+    {
+        private static readonly string TableName = "altitude_10m_mesh_registered";
+        public static readonly string ColumnMeshId = "mesh_id";
+        public static readonly string ColumnLowerLatitude = "lower_latitude";
+        public static readonly string ColumnLowerLongitude = "lower_longitude";
+        public static readonly string ColumnUpperLatitude = "upper_latitude";
+        public static readonly string ColumnUpperLongitude = "upper_longitude";
+        public static readonly string ColumnAltitude = "altitude";
+
+
+
+    }
+}
