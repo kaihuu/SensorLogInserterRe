379760ca4a84f541abb3497a9731f74966ee5f6d
Daiki <daiki.kawanuma@gmail.com>
2016-06-19 21:24:40 +0900

    modify AccCalculator

--- SensorLogInserterRe/Calculators/AccCalculator.cs
+++ SensorLogInserterRe/Calculators/AccCalculator.cs
@@ -10,10 +10,13 @@ namespace SensorLogInserterRe.Calculators
 {
     static class AccCalculator
     {
-        public static double CalcAcc(GeoCoordinate geoBefore, GeoCoordinate geoThis, GeoCoordinate geoAfter, double samplingTime)
+        public static double CalcAcc(double latitudeBefore, double longitudeBefore, 
+            double latitudeThis, double longitudeThis, 
+            double latitudeAfter, double longitudeAfter, double samplingTime)
         {
             //中間差分法による導出
-            return (HubenyDistanceCalculator.CalcHubenyFormula(geoThis, geoAfter) - HubenyDistanceCalculator.CalcHubenyFormula(geoBefore, geoThis)) / Math.Pow(samplingTime, 2);
+            return (DistanceCalculator.CalcDistance(latitudeThis, longitudeThis, latitudeAfter, longitudeAfter) 
+                - DistanceCalculator.CalcDistance(latitudeBefore, longitudeBefore, latitudeThis, longitudeBefore)) / Math.Pow(samplingTime, 2);
         }
     }
 }
