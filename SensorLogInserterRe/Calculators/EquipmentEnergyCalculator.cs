f6effde4f71b933566fef90afbdd7f9364330111
Daiki <daiki.kawanuma@gmail.com>
2016-06-19 21:47:17 +0900

    implement EquipmentEnergyCalculator

--- SensorLogInserterRe/Calculators/EquipmentEnergyCalculator.cs
+++ SensorLogInserterRe/Calculators/EquipmentEnergyCalculator.cs
@@ -8,10 +8,18 @@ namespace SensorLogInserterRe.Calculators
 {
     class EquipmentEnergyCalculator
     {
+        private static double OutwardEquipmentPower = 0.2;
+        private static double HomewardEquipmentPower = 0.3;
+
         public static double CalcEquipmentEnergy(DateTime time)
         {
-            // TODO 実装
-        }
+            // TODO サマータイムとか入れれば更にいいかもしれないけど、誤差の範囲内でしょうな
 
+            if (time.Hour < 18)
+                // [kWh/s]
+                return OutwardEquipmentPower / 3600;
+            else
+                return HomewardEquipmentPower / 3600;
+        }
     }
 }
