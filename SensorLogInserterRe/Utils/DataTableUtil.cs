eb0d044111f0a0c1c5fa2eb4fa1f64d8fc5f7ed9
Daiki-Kawanuma <daiki.kawanuma@gmail.com>
2016-06-23 19:59:14 +0900

    modify DataTableUtil

--- SensorLogInserterRe/Utils/DataTableUtil.cs
+++ SensorLogInserterRe/Utils/DataTableUtil.cs
@@ -15,15 +15,15 @@ namespace SensorLogInserterRe.Utils
             DataTable tripsRawTable = new DataTable();
 
             // TODO string 直書きでなく、Daoのstaticフィールドを参照に
-            tripsRawTable.Columns.Add(new DataColumn("DRIVER_ID"));
-            tripsRawTable.Columns.Add(new DataColumn("CAR_ID"));
-            tripsRawTable.Columns.Add(new DataColumn("SENSOR_ID"));
-            tripsRawTable.Columns.Add(new DataColumn("START_TIME"));
-            tripsRawTable.Columns.Add(new DataColumn("END_TIME"));
-            tripsRawTable.Columns.Add(new DataColumn("START_LATITUDE"));
-            tripsRawTable.Columns.Add(new DataColumn("START_LONGITUDE"));
-            tripsRawTable.Columns.Add(new DataColumn("END_LATITUDE"));
-            tripsRawTable.Columns.Add(new DataColumn("END_LONGITUDE"));
+            tripsRawTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("START_TIME", typeof(DateTime)));
+            tripsRawTable.Columns.Add(new DataColumn("END_TIME", typeof(DateTime)));
+            tripsRawTable.Columns.Add(new DataColumn("START_LATITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("START_LONGITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("END_LATITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("END_LONGITUDE", typeof(double)));
 
             return tripsRawTable;
         }
@@ -43,8 +43,9 @@ namespace SensorLogInserterRe.Utils
             tripsTable.Columns.Add(new DataColumn("START_LONGITUDE", typeof(double)));
             tripsTable.Columns.Add(new DataColumn("END_LATITUDE", typeof(double)));
             tripsTable.Columns.Add(new DataColumn("END_LONGITUDE", typeof(double)));
-            tripsTable.Columns.Add(new DataColumn("CONSUMED_ENERGY", typeof(double)));
+            tripsTable.Columns.Add(new DataColumn("CONSUMED_ENERGY", typeof(Single)));
             tripsTable.Columns.Add(new DataColumn("TRIP_DIRECTION", typeof(string)));
+            tripsTable.Columns.Add(new DataColumn("VALIDATION", typeof(string)));
 
             return tripsTable;
         }
@@ -77,9 +78,9 @@ namespace SensorLogInserterRe.Utils
             accRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
             accRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
             accRawTable.Columns.Add(new DataColumn("DATETIME", typeof(DateTime)));
-            accRawTable.Columns.Add(new DataColumn("ACC_X", typeof(double)));
-            accRawTable.Columns.Add(new DataColumn("ACC_Y", typeof(double)));
-            accRawTable.Columns.Add(new DataColumn("ACC_Z", typeof(double)));
+            accRawTable.Columns.Add(new DataColumn("ACC_X", typeof(Single)));
+            accRawTable.Columns.Add(new DataColumn("ACC_Y", typeof(Single)));
+            accRawTable.Columns.Add(new DataColumn("ACC_Z", typeof(Single)));
 
             return accRawTable;
         }
@@ -94,25 +95,25 @@ namespace SensorLogInserterRe.Utils
             correctedGpsTable.Columns.Add(new DataColumn("JST", typeof(DateTime)));
             correctedGpsTable.Columns.Add(new DataColumn("LATITUDE", typeof(double)));
             correctedGpsTable.Columns.Add(new DataColumn("LONGITUDE", typeof(double)));
-            correctedGpsTable.Columns.Add(new DataColumn("SPEED", typeof(double)));
-            correctedGpsTable.Columns.Add(new DataColumn("HEADING", typeof(double)));
-            correctedGpsTable.Columns.Add(new DataColumn("DISTANCE_DIFFERENCE", typeof(double)));
+            correctedGpsTable.Columns.Add(new DataColumn("SPEED", typeof(Single)));
+            correctedGpsTable.Columns.Add(new DataColumn("HEADING", typeof(Single)));
+            correctedGpsTable.Columns.Add(new DataColumn("DISTANCE_DIFFERENCE", typeof(Single)));
 
             return correctedGpsTable;
         }
 
         public static DataTable GetTempCorrectedAccTable(DataTable table)
         {
-            table.Columns.Add(new DataColumn("ROLL", typeof(double)));
-            table.Columns.Add(new DataColumn("PITCH", typeof(double)));
-            table.Columns.Add(new DataColumn("YAW", typeof(double)));
+            table.Columns.Add(new DataColumn("ROLL", typeof(Single)));
+            table.Columns.Add(new DataColumn("PITCH", typeof(Single)));
+            table.Columns.Add(new DataColumn("YAW", typeof(Single)));
 
             //補正データを記録するためのカラムを追加
-            table.Columns.Add(new DataColumn("ALPHA", typeof(double)));
-            table.Columns.Add(new DataColumn("VECTOR_X", typeof(double)));
-            table.Columns.Add(new DataColumn("VECTOR_Y", typeof(double)));
-            table.Columns.Add(new DataColumn("BETA", typeof(double)));
-            table.Columns.Add(new DataColumn("GAMMA", typeof(double)));
+            table.Columns.Add(new DataColumn("ALPHA", typeof(Single)));
+            table.Columns.Add(new DataColumn("VECTOR_X", typeof(Single)));
+            table.Columns.Add(new DataColumn("VECTOR_Y", typeof(Single)));
+            table.Columns.Add(new DataColumn("BETA", typeof(Single)));
+            table.Columns.Add(new DataColumn("GAMMA", typeof(Single)));
 
             return table;
         }
@@ -128,34 +129,34 @@ namespace SensorLogInserterRe.Utils
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnJst, typeof(DateTime)));
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLatitude, typeof(double)));
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLongitude, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnSpeed, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnHeading, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnDistanceDifference, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnTerraubAltitude, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnTerrainAltitudeDiffarencce, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLongitudinalAcc, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLateralAcc, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnVerticalAcc, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByAirResistance, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByRollingResistance, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByClimbingResistance, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByAccResistance, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConvertLoss, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRegeneLoss, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRegeneEnergy, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLostEnergy, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEfficiency, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedElectricEnergy, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLostEnergyByWellToWheel, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedFuel, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedFuelByWellToWheel, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByEquipment, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByCooling, typeof(double)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByHeating, typeof(double)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnSpeed, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnHeading, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnDistanceDifference, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnTerraubAltitude, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnTerrainAltitudeDiffarencce, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLongitudinalAcc, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLateralAcc, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnVerticalAcc, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByAirResistance, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByRollingResistance, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByClimbingResistance, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByAccResistance, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConvertLoss, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRegeneLoss, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRegeneEnergy, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLostEnergy, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEfficiency, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedElectricEnergy, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLostEnergyByWellToWheel, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedFuel, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnConsumedFuelByWellToWheel, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByEquipment, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByCooling, typeof(Single)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnEnergyByHeating, typeof(Single)));
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnTripDirection, typeof(string)));
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnMeshId, typeof(int)));
             ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnLinkId, typeof(string)));
-            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRoadTheta, typeof(double)));
+            ecologTable.Columns.Add(new DataColumn(EcologDao.ColumnRoadTheta, typeof(Single)));
 
             return ecologTable;
         }
