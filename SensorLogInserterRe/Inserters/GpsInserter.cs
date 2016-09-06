e2ee37b95ed129c50292d73c4b727776ae7643df
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-09-06 17:00:54 +0900

    modify SpeedCalculator

--- SensorLogInserterRe/Inserters/GpsInserter.cs
+++ SensorLogInserterRe/Inserters/GpsInserter.cs
@@ -115,7 +115,9 @@ namespace SensorLogInserterRe.Inserters
                     gpsRawTable.Rows[i - 1].Field<DateTime>(AndroidGpsRawDao.ColumnJst),
                     gpsRawTable.Rows[i + 1].Field<double>(AndroidGpsRawDao.ColumnLatitude),
                     gpsRawTable.Rows[i + 1].Field<double>(AndroidGpsRawDao.ColumnLongitude),
-                    gpsRawTable.Rows[i + 1].Field<DateTime>(AndroidGpsRawDao.ColumnJst)));
+                    gpsRawTable.Rows[i + 1].Field<DateTime>(AndroidGpsRawDao.ColumnJst),
+                    gpsRawTable.Rows[i].Field<double>(AndroidGpsRawDao.ColumnLatitude),
+                    gpsRawTable.Rows[i].Field<double>(AndroidGpsRawDao.ColumnLongitude)));
 
                 //速度が1km以上になったらHEADINGを更新する(停止時に1つ1つ計算するとHEADINDが暴れるため)
                 if (row.Field<Single>(CorrectedGpsDao.ColumnSpeed) > 1.0)
