9ae18ebc1293f0ccf9d9e7701bcb9c8a0bde4548
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-09-06 16:56:01 +0900

    modify SpeedCalculator

--- SensorLogInserterRe/Calculators/SpeedCalculator.cs
+++ SensorLogInserterRe/Calculators/SpeedCalculator.cs
@@ -11,12 +11,14 @@ namespace SensorLogInserterRe.Calculators
    　static class SpeedCalculator
     {
         /** km/h の速度を返す **/
-        public static double CalcSpeed(double latitudeBefore, double longitudeBefore, DateTime timeBefore, double latitudeAfter, double longitudeAfter, DateTime timeAfter)
+        public static double CalcSpeed(double latitudeBefore, double longitudeBefore, DateTime timeBefore, double latitudeAfter, double longitudeAfter, DateTime timeAfter,
+            double latitudeFocused, double longitudeFocused)
         {
             //中間差分法を用いた導出
             //return DistanceCalculator.CalcDistance(latitudeBefore, longitudeBefore, latitudeAfter, longitudeAfter) / 2 / samplingSeconds * 3.6;
 
-            return DistanceCalculator.CalcDistance(latitudeBefore, longitudeBefore, latitudeAfter, longitudeAfter) /
+            return (DistanceCalculator.CalcDistance(latitudeBefore, longitudeBefore, latitudeFocused, longitudeFocused) + 
+                DistanceCalculator.CalcDistance(latitudeFocused, longitudeFocused, latitudeAfter, longitudeAfter)) /
                    (timeAfter - timeBefore).TotalSeconds * 3.6;
         }
     }
