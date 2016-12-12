1aec3ec101cdbd2ce77f9cec95fa2af44e777eca
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-12-12 20:19:05 +0900

    Add LinksForSearchDao

---
+++ SensorLogInserterRe/Daos/LinksForSearchDao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Daos
+{
+    class LinksForSearchDao
+    {
+
+            private static readonly string TableName = "links_for_search";
+
+            public static DataTable Get()
+            {
+                string query = $"SELECT * FROM {TableName}";
+
+                return DatabaseAccesser.GetResult(query);
+            }
+
+            public static DataTable Get(string linkId)
+            {
+                string query = "SELECT * ";
+                query += "FROM '{TableName}' ";
+                query += $"WHERE link_id = '{linkId}' ";
+
+                return DatabaseAccesser.GetResult(query);
+            }
+
+        }
+    
+}
