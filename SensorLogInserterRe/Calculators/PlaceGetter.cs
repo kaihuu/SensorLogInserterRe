754c5d9a3511582b4803104f3f72833efbd6f034
kaihuu <uemura-toshiaki-xh@ynu.jp>
2018-04-06 15:05:07 +0900

    add placeDao

---
+++ SensorLogInserterRe/Calculators/PlaceGetter.cs
@@ -0,0 +1,57 @@
+﻿using SensorLogInserterRe.Daos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Calculators
+{
+    class PlaceGetter
+    {
+        private static PlaceGetter _instance;
+
+        private DataTable _placeTable;
+
+        private PlaceGetter()
+        {
+            // for Singleton pattern
+        }
+
+        public static PlaceGetter GetInstance()
+        {
+            if (_instance == null)
+            {
+                InitInstance();
+            }
+
+            return _instance;
+        }
+
+        private static void InitInstance()
+        {
+            _instance = new PlaceGetter
+            {
+                _placeTable = PlaceDao.Get()
+            };
+
+        }
+
+        public Tuple<GeoCoordinate, GeoCoordinate> Get(String placeName)
+        {
+            GeoCoordinate startCoordinate = new GeoCoordinate();
+            GeoCoordinate endCoordinate = new GeoCoordinate();
+            DataRow dataRow = _placeTable.Select("place_name = "+ placeName)[0];
+
+            startCoordinate.Latitude = dataRow.Field<double>(PlaceDao.ColumnStartLatitude);
+            startCoordinate.Longitude = dataRow.Field<double>(PlaceDao.ColumnStartLongitude);
+            endCoordinate.Latitude = dataRow.Field<double>(PlaceDao.ColumnEndLatitude);
+            endCoordinate.Longitude = dataRow.Field<double>(PlaceDao.ColumnEndLongitude);
+
+
+            return Tuple.Create(startCoordinate, endCoordinate);
+        }
+    }
+}
---
+++ SensorLogInserterRe/Daos/PlaceDao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorLogInserterRe.Daos
+{
+    class PlaceDao
+    {
+        public static readonly string ColumnPlaceID = "place_id";
+        public static readonly string ColumnPlaceName = "place_name";
+        public static readonly string ColumnStartLatitude = "start_latitude";
+        public static readonly string ColumnEndLatitude = "end_latitude";
+        public static readonly string ColumnStartLongitude = "start_longitude";
+        public static readonly string ColumnEndLongitude = "end_longitude";
+        public static readonly string ColumnStartDate = "start_date";
+        public static readonly string ColumnEndDate = "end_date";
+
+
+        private static readonly string TableName = "place";
+
+        public static void Insert(DataTable dataTable)
+        {
+            DatabaseAccesser.Insert(TableName, dataTable);
+        }
+
+        public static DataTable Get()
+        {
+            string query = "SELECT * FROM " + TableName;
+
+            return DatabaseAccesser.GetResult(query);
+        }
+        public static int GetPlace(string placeName)
+        {
+            var query = new StringBuilder();
+            query.AppendLine("SELECT * ");
+            query.AppendLine($"FROM {TableName}");
+            query.AppendLine($"WHERE place_name = '{placeName}'");
+
+            return DatabaseAccesser.GetResult(query.ToString())
+                .AsEnumerable()
+                .Select(v => v.Field<int?>(EfficiencyDao.ColumnEfficiency)).FirstOrDefault() ?? -1;
+        }
+
+    }
+}
